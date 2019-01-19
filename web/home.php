76b09c4f60c63bc0e9e9b3b31086eec0b0afc10b
spencer <pec15002@byui.edu>
2019-01-19 16:43:49 -0700

    added home page

---
+++ web/home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://safe-falls-66937.herokuapp.com/home.css">
+	<title>Spencer's Home</title>
+	<style type="text/css"></style>
+</head>
+<body>
+	<h1>Welcome to Spencer Peck's home page
+            </h1>
+            <div>
+                My name is Spencer Peck and I am from sandy Utah.<br>  I am majoring in Software Engineering.
+                
+            </div>
+            
+            <a href="http://157.201.194.254/viper2226/index.html">CS 213 Assignments</a>
+            
+            <img src="http://www.clker.com/cliparts/W/d/I/m/5/t/fam-tree.svg"
+                 alt="black tree" style="width:400px;height:300px;"/>
+            
+<blockquote>
+“No misfortune is so bad that whining about it won’t make it worse. (Apr 2007 Gen Conf)” <br> ― Jeffrey R. Holland
+</blockquote>
+	
+</body>
+</html>
