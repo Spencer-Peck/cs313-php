6c28b7b9d3f71c370a795033282195b65311abb2
spencer <pec15002@byui.edu>
2019-01-19 17:54:03 -0700

    added stuff

---
+++ web/assignments.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Assignments</title>
+	<style type="text/css"></style>
+</head>
+<body>
+<h1>Coming soon!!</h1>
+            
+	
+</body>
+</html>
--- web/home.php
+++ web/home.php
@@ -6,12 +6,7 @@
 	<style type="text/css"></style>
 </head>
 <body>
-	<ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+
 	<h1>Welcome to Spencer Peck's home page
             </h1>
             <div>
@@ -19,7 +14,7 @@
                 
             </div>
             
-            <a href="http://157.201.194.254/viper2226/index.html">CS 213 Assignments</a>
+            <a href="https://safe-falls-66937.herokuapp.com/assignments.php">CS 313 Assignments</a>
             
             <img src="http://www.clker.com/cliparts/W/d/I/m/5/t/fam-tree.svg"
                  alt="black tree" style="width:400px;height:300px;"/>
