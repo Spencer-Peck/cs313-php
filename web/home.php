9ef14c1403d9e422b2f1a62c602fc17aa8c594f4
spencer <pec15002@byui.edu>
2019-01-19 17:05:39 -0700

    added menu bar

--- web/home.php
+++ web/home.php
@@ -6,6 +6,12 @@
 	<style type="text/css"></style>
 </head>
 <body>
+	<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
 	<h1>Welcome to Spencer Peck's home page
             </h1>
             <div>
