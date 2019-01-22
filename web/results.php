3c85b0c272fbaa56d32415ee956de9dfb6cc80d2
spencer <pec15002@byui.edu>
2019-01-22 10:55:38 -0700

    creat form and results page

---
+++ web/results.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css"></style>
+</head>
+<body>
+  <?php $email = $_POST["email"]; ?>
+
+  Name <?php echo $_POST["name"]; ?><br>
+  Email <?php echo "<a href = 'mailto: $email'>$email</a>" ?><br>
+  Major: <?php echo $_POST["majors"]; ?><br>
+  Comments: <?php echo $_POST["name"]; ?><br>
+	
+</body>
+</html>
