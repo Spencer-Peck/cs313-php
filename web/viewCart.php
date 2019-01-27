32883bf10258ccbe8dc8e7486dd073fa1fda0157
spencer <pec15002@byui.edu>
2019-01-27 11:17:21 -0700

    added product form test

---
+++ web/viewCart.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css"></style>
+</head>
+<body>
+
+  peanut <?php echo $_SESSION["qPeanut"]; ?><br>
+
+
+	
+</body>
+</html>
