25c710cc944d6046fef8679b6cfc24c4f7361186
spencer <pec15002@byui.edu>
2019-01-27 12:38:10 -0700

    added product form test

--- web/viewCart.php
+++ web/viewCart.php
@@ -12,7 +12,7 @@ session_start();
   <?php
   echo "<ul>";
   if ($_SESSION["peanut"] == true){
-    echo "<li> Box of Peanut Butter Cookies  Quantity:" . $_SESSION['qPeanut'] . "<br> subtotal: ";
+    echo "<li> Box of Peanut Butter Cookies <br>  Quantity: " . $_SESSION['qPeanut'] . "<br> Subtotal: ";
     echo "$ " . $_SESSION["qPeanut"] * 3.99 . "</li>";
   }
   echo "</ul>";
