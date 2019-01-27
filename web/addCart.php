2310093797a708cf21c7a038eed908d61b9810fe
spencer <pec15002@byui.edu>
2019-01-27 09:57:53 -0700

    added product form test

--- web/addCart.php
+++ web/addCart.php
@@ -8,22 +8,22 @@
   cart <?php echo $_POST["cart"]; ?><br>
 
   <?php
-  $myCart = $_POST["cart"]
+  $myCart = $_POST["cart"];
 
   if ($myCart == "peanut"){
   	$_SESSION["peanut"] = "true";
-  	echo $_POST["cart"]
+  	echo $_POST["cart"];
   }
 
   if ($myCart == "chip"){
   	$_SESSION["chip"] = "true";
-  	echo $_POST["cart"]
+  	echo $_POST["cart"];
 
   }
 
   if ($myCart == "oatmeal"){
   	$_SESSION["oatmeal"] = "true";
-  	echo $_POST["cart"]
+  	echo $_POST["cart"];
 
   }
 
