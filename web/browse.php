4cc6f47c2b98d2acf38140f6bb143611c017487e
spencer <pec15002@byui.edu>
2019-01-26 19:23:38 -0700

    added product form test

--- web/browse.php
+++ web/browse.php
@@ -74,8 +74,9 @@
   <ul class="price">
     <li class="header" style="background-color:#663203">Peanut Butter</li>
     <li class="grey">$ 3.99</li>
-    <li><img src="https://www.modernhoney.com/wp-content/uploads/2018/10/Soft-Chewy-Peanut-Butter-Cookies-4-cr.jpg" alt="Chocolate chip" style="width:400px;height:200px;"></li>
-    <li class="grey">Quantity<input type="number" id="myNumber" value="1"><a href="#" class="button">Add to cart</a></li>
+    <li><img src="https://www.modernhoney.com/wp-content/uploads/2018/10/Soft-Chewy-Peanut-Butter-Cookies-4-cr.jpg" alt="Chocolate chip" style="width:350px;height:200px;"></li>
+    <li>Quantity<input type="number" id="myNumber" value="1"></li>
+    <li class="grey"><a href="#" class="button">Add to cart</a></li>
   </ul>
 </div>
 
@@ -83,8 +84,9 @@
   <ul class="price">
     <li class="header" style="background-color:#c47a36">Oatmeal</li>
     <li class="grey">$ 4.99</li>
-    <li><img src="https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2017/01/Soft-and-Chewy-Oatmeal-Raisin-Cookies-3.jpg?resize=1360%2C2040" alt="Chocolate chip" style="width:400px;height:200px;"></li>
-    <li class="grey">Quantity<input type="number" id="myNumber" value="1"><a href="#" class="button">Add to cart</a> </li>
+    <li><img src="https://i2.wp.com/www.livewellbakeoften.com/wp-content/uploads/2017/01/Soft-and-Chewy-Oatmeal-Raisin-Cookies-3.jpg?resize=1360%2C2040" alt="Chocolate chip" style="width:350px;height:200px;"></li>
+    <li>Quantity<input type="number" id="myNumber" value="1"></li>
+    <li class="grey"><a href="#" class="button">Add to cart</a> </li>
   </ul>
 </div>
 
@@ -92,7 +94,7 @@
   <ul class="price">
     <li class="header" style="background-color:#663203">Chocolate Chip</li>
     <li class="grey">$ 3.45</li>
-    <li><img src="https://www.handletheheat.com/wp-content/uploads/2018/02/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9.jpg" alt="Chocolate chip" style="width:400px;height:200px;"></li>
+    <li><img src="https://www.handletheheat.com/wp-content/uploads/2018/02/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9.jpg" alt="Chocolate chip" style="width:350px;height:200px;"></li>
     <li>Quantity<input type="number" id="myNumber" value="1"></li>
 
     <li class="grey"><a href="#" class="button">Add to cart</a></li>
