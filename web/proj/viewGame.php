89a0aa053472d888442d69205d13e9a10a71d26e
spencer <pec15002@byui.edu>
2019-02-01 02:36:40 -0700

    added commands.sql

--- web/proj/viewGame.php
+++ web/proj/viewGame.php
@@ -17,11 +17,12 @@
 
     <?php
     $game_id = $_GET["game_id"];
-    foreach ($db->query('SELECT winner_name, score, session_description, date FROM game_session WHERE game_id =' . $game_id . ' ORDER BY date DESC') as $row)
+    $name = $_GET["name"];
+    foreach ($db->query('SELECT session_id, winner_name, score, session_description, date FROM game_session WHERE game_id =' . $game_id . ' ORDER BY date DESC') as $row)
     {
         echo '<tr><td>' . $row['winner_name'] . '</td><td>' . $row['score'] . '</td>' .
         '<td>' . $row['date'] . '</td>';
-        echo '<td><button type="button" data-toggle="confirmation" class="btn btn-danger">Delete</button></td></tr>';
+        echo '<td><button href="add_delete.php?game_id=$game_id&name=$name&session_id=' . $row['session_id'] .'&action=delete" type="button" class="btn btn-danger" data-toggle="confirmation">Delete</button></td></tr>';
 
     }
 
