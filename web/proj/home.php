08479cd6250f73d0fb85300360eaf90c5e49a3be
spencer <pec15002@byui.edu>
2019-01-31 18:01:58 -0700

    added commands.sql

--- web/proj/home.php
+++ web/proj/home.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
@@ -23,6 +24,15 @@
     </ul>
   </div>
 </nav>
+
+<div class="container">
+  <h2>List Group With Linked Items</h2>
+  <div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action">First item</a>
+    <a href="#" class="list-group-item list-group-item-action">Second item</a>
+    <a href="#" class="list-group-item list-group-item-action">Third item</a>
+  </div>
+</div>
 
 	
 </body>
