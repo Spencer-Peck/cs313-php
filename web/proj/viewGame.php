5de89446950252205746ee1cd7b809917f6d01ee
spencer <pec15002@byui.edu>
2019-02-01 16:26:45 -0700

    added commands.sql

--- web/proj/viewGame.php
+++ web/proj/viewGame.php
@@ -2,7 +2,7 @@
 <?php include 'order.php';?>
 
 
-<div class="container">
+<div class="container-fluid">
 <div class="row"><div class="col-sm"><h2><?php echo $_GET["name"];?></h2></div> <div class="col-sm" class="text-right"><a href="#" class="btn btn-success " role="button" >Add Score</a></div></div>         
   <table class="table table-striped" id="games">
     <thead>
