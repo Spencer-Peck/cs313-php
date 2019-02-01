d1d0d284841809580c1cd7b14b22bfedf246bdf1
spencer <pec15002@byui.edu>
2019-02-01 02:46:10 -0700

    added commands.sql

--- web/proj/add_delete.php
+++ web/proj/add_delete.php
@@ -1,1 +1,16 @@
-<?php include 'db_connect.php';?>
+<?php include 'db_connect.php';
+
+$action = $_GET["action"];
+$name = $_GET["name"];
+$game_id = $_GET["game_id"];
+if ($action = delete){
+	$session_id = $_GET["session_id"];
+	$stmt = $db->prepare('DELETE FROM game_session WHERE session_id = ' . $session_id);
+	$stmt->execute();
+}
+$page = "viewGame.php?game_id=$game_id&name=$name";
+
+header("location: $page");
+die();
+
+?>
