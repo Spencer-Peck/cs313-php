0dd0a6015a261b218d4228e229b8789a6ab7c911
spencer <pec15002@byui.edu>
2019-01-31 19:59:07 -0700

    added commands.sql

--- web/proj/viewGame.php
+++ web/proj/viewGame.php
@@ -18,8 +18,7 @@
     foreach ($db->query('SELECT winner_name, score, session_description, date FROM game_session WHERE game_id = 1') as $row)
     {
         echo '<tr><td>' . $row['winner_name'] . '</td><td>' . $row['score'] . '</td>' .
-        '<td>' . $row['winner_name'] . '</td><td>' . $row['session_description'] . '</td><td>' .
-        $row['date'] . '</td></tr>';
+        '<td>' . $row['session_description'] . '</td><td>' . $row['date'] . '</td></tr>';
 
     }
 
