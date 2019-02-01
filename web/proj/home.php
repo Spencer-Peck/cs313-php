720e3b0a02e735d1b5fe60ef6520ee2d314bd39b
spencer <pec15002@byui.edu>
2019-01-31 18:40:18 -0700

    added commands.sql

--- web/proj/home.php
+++ web/proj/home.php
@@ -62,11 +62,11 @@ catch (PDOException $ex)
     <?php
     foreach ($db->query('SELECT game_name FROM games') as $row)
     {
-        echo '<a href="#" class="list-group-item list-group-item-action">' . $row['game_name'] . '</a>'
+        echo '<a href="#" class="list-group-item list-group-item-action">' . $row['game_name'] . '</a>';
     }
 
     ?>
-    
+
   </div>
 </div>
 
