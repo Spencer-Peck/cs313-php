d8ea7645b12615d483855031340ed294a70392cc
spencer <pec15002@byui.edu>
2019-02-01 02:00:50 -0700

    added commands.sql

--- web/proj/header.php
+++ web/proj/header.php
@@ -9,13 +9,17 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-  <script src="path/to/jquery.js"></script>
-  <script src="path/to/popper.js"></script>
-  <script src="path/to/bootstrap.js"></script>
-  <script src="path/to/bootstrap-confirmation.js"></script>
+  <script type="text/javascript" src="path/to/jquery.js"></script>
+<script type="text/javascript" src="path/to/bootstrap-tooltip.js"></script>
+<script type="text/javascript" src="path/to/bootstrap-confirmation.js"></script> 
+<script src="path/to/jquery.js"></script>
+<script src="path/to/popper.js"></script>
+<script src="path/to/bootstrap.js"></script>
+<script src="path/to/bootstrap-confirmation.js"></script>
 </head>
+
 <body>
-<?php include 'https://cdn.jsdelivr.net/npm/bootstrap-confirmation2/dist/bootstrap-confirmation.min.js';?>
+<?php include 'bootstrap-confirmation.min.js';?>
 <?php include 'db_connect.php';?>
 
 
