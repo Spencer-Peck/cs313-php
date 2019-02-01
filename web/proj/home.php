51d1d404b05e09afb7b16a4ee1ead8bd554a01e5
spencer <pec15002@byui.edu>
2019-01-31 18:12:19 -0700

    added commands.sql

--- web/proj/home.php
+++ web/proj/home.php
@@ -13,18 +13,15 @@
 <body>
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <!-- Brand/logo -->
-  <a class="navbar-brand" href="#">Logo</a>
+  <a class="navbar-brand" href="#">Score Keeper</a>
   
   <!-- Links -->
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#">Link 1</a>
+      <a class="nav-link" href="#">Add/Edit Games</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Link 2</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Link 3</a>
+      <a class="nav-link" href="#">Add/Edit Sessions</a>
     </li>
   </ul>
 </nav>
