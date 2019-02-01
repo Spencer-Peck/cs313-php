3cea5b2ae3fd9b7149fc3b757409298800092296
spencer <pec15002@byui.edu>
2019-02-01 11:28:13 -0700

    added commands.sql

--- web/proj/score_fetch.php
+++ web/proj/score_fetch.php
@@ -3,5 +3,5 @@
 echo '<a href="#demo" data-toggle="collapse"><tr>';
 echo '<td>' . $row['winner_name'] . '</td><td>' . $row['score'] . '</td>' . '<td>' . $row['date'] . '</td>';
 echo '<td><a href="add_delete.php?game_id='.$game_id.'&name='.$name.'&session_id=' . $row['session_id'] .'&action=delete" role="button" class="btn btn-danger">Delete</a></td></tr></a>';
-echo '<tr class="collapse">'. row['session_description'] . '</tr>';
+echo '<tr class="collapse">'. $row['session_description'] . '</tr>';
 ?>
