11debdf2fe3c2c1137116d26fd56893306c85a0f
spencer <pec15002@byui.edu>
2019-02-01 19:19:20 -0700

    added commands.sql

--- web/proj/add_delete.php
+++ web/proj/add_delete.php
@@ -5,7 +5,7 @@ $name = $_GET["name"];
 $game_id = $_GET["game_id"];
 $winner = $_GET["win_name"];
 $score = $_GET["score"];
-$theDate = date("Y-m-d");
+//$theDate = date("Y-m-d");
 if ($action == "delete"){
 	$session_id = $_GET["session_id"];
 	$stmt = $db->prepare('DELETE FROM game_session WHERE session_id = ' . $session_id);
@@ -13,12 +13,12 @@ if ($action == "delete"){
 }
 
 if ($action == "add"){
-	$stmt = $db->prepare('INSERT INTO game_session (game_id, winner_name, score, date) VALUES  (:game_id, :winner, :score, :adate)');
-	$stmt->execute();
+	$stmt = $db->prepare('INSERT INTO game_session (game_id, winner_name, score, date) VALUES  (:game_id, :winner, :score)');
 	$stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
 	$stmt->bindValue(':winner', $winner, PDO::PARAM_STR);
 	$stmt->bindValue(':score', $score, PDO::PARAM_STR);
-	$stmt->bindValue(':adate', $theDate, PDO::PARAM_STR);
+	//$stmt->bindValue(':adate', $theDate, PDO::PARAM_STR);
+	$stmt->execute();
 }
 
 
