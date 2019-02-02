894482ac56cfa76c6ccd4505a71f776c53b99940
spencer <pec15002@byui.edu>
2019-02-01 19:22:36 -0700

    added commands.sql

--- web/proj/add_delete.php
+++ web/proj/add_delete.php
@@ -6,13 +6,13 @@ $game_id = $_GET["game_id"];
 $winner = $_GET["win_name"];
 $score = $_GET["score"];
 //$theDate = date("Y-m-d");
-if ($action == "delete"){
+if ($action == 'delete'){
 	$session_id = $_GET["session_id"];
 	$stmt = $db->prepare('DELETE FROM game_session WHERE session_id = ' . $session_id);
 	$stmt->execute();
 }
 
-if ($action == "add"){
+if ($action == 'add'){
 	$stmt = $db->prepare('INSERT INTO game_session (game_id, winner_name, score, date) VALUES  (:game_id, :winner, :score)');
 	$stmt->bindValue(':game_id', $game_id, PDO::PARAM_INT);
 	$stmt->bindValue(':winner', $winner, PDO::PARAM_STR);
