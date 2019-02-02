1342757f9dcd95f13d59966d0e841c44e0a8aa94
spencer <pec15002@byui.edu>
2019-02-01 19:39:45 -0700

    added commands.sql

--- web/proj/add_score_form.php
+++ web/proj/add_score_form.php
@@ -3,11 +3,11 @@
 <form action=<?php echo 'add_delete.php?game_id='.$_GET["game_id"].'&action=add';?> method = "post">
 	<div class="form-group">
     <label for="win_name">Name:</label>
-    <input type="text" class="form-control" id="win_name">
+    <input type="text" name="win_name" class="form-control" id="win_name">
   </div>
   <div class="form-group">
     <label for="score">Score:</label>
-    <input type="text" class="form-control" id="score">
+    <input type="text" name="score" class="form-control" id="score">
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
