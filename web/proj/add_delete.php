5e211bb55fadc177578f50690a263071f507ecfb
spencer <pec15002@byui.edu>
2019-02-01 18:47:52 -0700

    added commands.sql

--- web/proj/add_delete.php
+++ web/proj/add_delete.php
@@ -3,11 +3,21 @@
 $action = $_GET["action"];
 $name = $_GET["name"];
 $game_id = $_GET["game_id"];
-if ($action = delete){
+$winner = $_GET["win_name"];
+$score = $_GET["score"];
+if ($action == "delete"){
 	$session_id = $_GET["session_id"];
 	$stmt = $db->prepare('DELETE FROM game_session WHERE session_id = ' . $session_id);
 	$stmt->execute();
 }
+
+if ($action == "add"){
+	$stmt = $db->prepare('INSERT INTO game_session (game_id, winner_name, score, date) VALUES  ('.$game_id.', "'.$winner.'", "'.$score.'", "'.date("Y/m/d").'")')
+	$stmt->execute();
+}
+
+
+
 $page = "viewGame.php?game_id=$game_id&name=$name";
 
 header("location: $page");
--- web/proj/add_score_form.php
+++ web/proj/add_score_form.php
@@ -2,8 +2,8 @@
 <div class="container-fluid">
 <form action=<?php echo 'add_delete.php?game_id='.$_GET["game_id"].'&action=add';?> method = "post">
 	<div class="form-group">
-    <label for="name">Name:</label>
-    <input type="text" class="form-control" id="name">
+    <label for="win_name">Name:</label>
+    <input type="text" class="form-control" id="win_name">
   </div>
   <div class="form-group">
     <label for="score">Score:</label>
