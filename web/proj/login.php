d2845bd58f596b5bab2d9e6ebab1ea959279dfef
spencer <pec15002@byui.edu>
2019-02-02 22:55:07 -0700

    added commands.sql

--- web/proj/login.php
+++ web/proj/login.php
@@ -6,7 +6,7 @@ $myusername = $_POST['username'];
 $mypassword = $_POST['pwd'];
 $count = 0;
 
-//$statement = $db->query('SELECT user_id FROM users WHERE user_name = :username AND password = :password');
+$statement = $db->query('SELECT user_id FROM users WHERE user_name = :username AND password = :password');
 //$statement->bindValue(':username', $myusername, PDO::PARAM_STR);
 //$statement->bindValue(':password', $mypassword, PDO::PARAM_STR);
 //$results = $statement->fetchAll(PDO::FETCH_ASSOC);
