a8db8b0d42112ee6b2a583a7c4d74a3badc56a99
spencer <pec15002@byui.edu>
2019-02-03 02:00:17 -0700

    added commands.sql

--- web/proj/home.php
+++ web/proj/home.php
@@ -8,7 +8,7 @@
     $user_id = $_SESSION['user_id'];
     foreach ($db->query('SELECT game_name, game_id FROM games WHERE user_id = ' . $user_id) as $row)
     {
-        echo '<a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '</a><button type="button" class="btn btn-danger">Danger</button>';
+        echo '<a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '<button type="button" class="btn btn-danger">Danger</button></a>';
     }
 
     ?>
