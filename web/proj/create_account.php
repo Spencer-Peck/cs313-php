aa9d498a88f63a3fa7299bb970f77f2c0171af93
spencer <pec15002@byui.edu>
2019-02-03 01:30:08 -0700

    added commands.sql

---
+++ web/proj/create_account.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'db_connect.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['pwd'];
+
+
+    if (isset($username) && isset($password)){
+    $stmt = $db->prepare('INSERT INTO users (user_name, password) VALUES  (:username, :password)');
+	$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+	$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+	$stmt->execute();
+}else{
+	header("location:account_form.php");
+      die();
+}
+
+
+
+
+?>
