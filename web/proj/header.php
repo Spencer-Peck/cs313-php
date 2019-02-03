b116c7023c072f5fdbf064608fde4b4e923e8378
spencer <pec15002@byui.edu>
2019-02-03 00:26:59 -0700

    added commands.sql

--- web/proj/header.php
+++ web/proj/header.php
@@ -33,7 +33,7 @@
       <a class="nav-link" href="#">Add/Edit Games</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Log Out</a>
+      <a class="nav-link" href="#">Log out</a>
     </li>
   </ul>
 </nav>
