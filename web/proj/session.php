29b9fb6d3e6659cef9b996398e24b2ef17789b80
spencer <pec15002@byui.edu>
2019-02-03 01:13:55 -0700

    added commands.sql

--- web/proj/session.php
+++ web/proj/session.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['login_user'])){
    }
 
 
-   if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 5)) {
+   if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
     // last request was more than 30 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
