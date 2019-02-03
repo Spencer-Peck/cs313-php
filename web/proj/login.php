e0651651b7bcf4a7873104790f48c17e0c1b9e80
spencer <pec15002@byui.edu>
2019-02-02 22:49:49 -0700

    added commands.sql

--- web/proj/login.php
+++ web/proj/login.php
@@ -3,18 +3,18 @@
 session_start();
 
 $myusername = $_POST['username'];
-$mypassword = $_POST['password'];
+$mypassword = $_POST['pwd'];
 
 
 $statement = $db->query('SELECT user_id FROM users WHERE user_name = :username AND password = :password');
 $statement->bindValue(':username', $myusername, PDO::PARAM_STR);
 $statement->bindValue(':password', $mypassword, PDO::PARAM_STR);
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-//$count = pg_num_rows($results);
+$count = pg_num_rows($results);
 
 if($count == 1) {
     $_SESSION['login_user'] = $myusername;
-    //$_SESSION['user_id'] = $results['user_id']
+    $_SESSION['user_id'] = $results['user_id']
     header("location: home.php");
 }else {
     $error = "Your Login Name or Password is invalid";
