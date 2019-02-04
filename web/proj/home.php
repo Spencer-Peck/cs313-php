a18e00c945b04e0f67fc254b6c27751b6adfb4bd
spencer <pec15002@byui.edu>
2019-02-04 11:16:06 -0700

    added commands.sql

--- web/proj/home.php
+++ web/proj/home.php
@@ -1,19 +1,30 @@
 <?php include 'header.php';?>
 
+
+
+
 <div class="container">
-  <h2>Games:</h2>
-  <div class="list-group">
+
+  <table class="table table-striped" id="games">
+    <thead>
+      <tr>
+        <th>Name </th>
+        <th>View Scores</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
 
     <?php
     $user_id = $_SESSION['user_id'];
     foreach ($db->query('SELECT game_name, game_id FROM games WHERE user_id = ' . $user_id) as $row)
     {
-        echo '<a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '</a>';
+        echo '<tr><td><a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '</a></td><td> a href="" role="button" class="btn btn-danger">Delete</a></td></tr>';
     }
 
     ?>
-
-  </div>
+    </tbody>
+  </table>
 </div>
 
 	
