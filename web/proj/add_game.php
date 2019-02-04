d443c7d90f8c8b9ebfe2fbf853ffe0cad99eb60e
spencer <pec15002@byui.edu>
2019-02-04 11:56:45 -0700

    added commands.sql

---
+++ web/proj/add_game.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'session.php';
+
+    $game_name = $_POST["game_name"];
+    $user_id = $_SESSION['user_id']
+
+
+    $stmt = $db->prepare('INSERT INTO games (user_id, game_name) VALUES  (:user_id, :game_name)');
+	$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+	$stmt->bindValue(':game_name', $game_name, PDO::PARAM_STR);
+	$stmt->execute();
+
+?>
