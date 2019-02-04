b4d124be33b9221929a5e82e5d21cd69c048c38a
spencer <pec15002@byui.edu>
2019-02-04 11:37:47 -0700

    added commands.sql

---
+++ web/proj/add_game_form.php
@@ -0,0 +1,16 @@
+<?php include 'header.php';
+?>
+<div class="container-fluid">
+<form action="add_game.php" method="post">
+	<div class="form-group">
+    <label for="game_name">Game Name:</label>
+    <input type="text" name="game_name" class="form-control" id="game_name">
+  </div>
+
+  <button type="submit" class="btn btn-primary" value="Submit">Add Game</button>
+</form>
+</div>
+
+
+</body>
+</html>
