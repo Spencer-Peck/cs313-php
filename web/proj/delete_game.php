e2e91696ea4b40b8671e939b641a860645ab03e5
spencer <pec15002@byui.edu>
2019-02-04 14:43:39 -0700

    added commands.sql

--- web/proj/delete_game.php
+++ web/proj/delete_game.php
@@ -5,4 +5,5 @@ $game_id = $_GET["game_id"];
 //echo '<h1>'.$score.'</h1>';
 	$stmt = $db->prepare('DELETE FROM games WHERE game_id = ' . $game_id);
 	$stmt->execute();
+	header("location:home.php");
 ?>
