9833d9a489e454a5384ff2d66541bb0eaf424ddf
spencer <pec15002@byui.edu>
2019-02-04 10:27:26 -0700

    added commands.sql

--- web/proj/logout.php
+++ web/proj/logout.php
@@ -3,6 +3,6 @@
    
    if(session_destroy()) {
    	session_unset();     // unset $_SESSION variable for the run-time 
-      header("Location: login.php");
+      header("Location: login_form.php");
    }
 ?>
