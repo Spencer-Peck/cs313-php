860658ecba55f6d3046a8fbd25444b3cdb529265
spencer <pec15002@byui.edu>
2019-02-04 14:42:26 -0700

    added commands.sql

---
+++ web/proj/delete_game.php
@@ -0,0 +1,8 @@
+<?php include 'session.php';;
+
+$game_id = $_GET["game_id"];
+
+//echo '<h1>'.$score.'</h1>';
+	$stmt = $db->prepare('DELETE FROM games WHERE game_id = ' . $game_id);
+	$stmt->execute();
+?>
--- web/proj/home.php
+++ web/proj/home.php
@@ -19,7 +19,7 @@
     $user_id = $_SESSION['user_id'];
     foreach ($db->query('SELECT game_name, game_id FROM games WHERE user_id = ' . $user_id) as $row)
     {
-        echo '<tr><td><a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '</a></td><td> <a href="" role="button" class="btn btn-danger">Delete</a></td></tr>';
+        echo '<tr><td><a href="viewGame.php?game_id=' . $row['game_id'] .'&name=' . $row['game_name'] . '"class="list-group-item list-group-item-action">' . $row['game_name'] . '</a></td><td> <a href="delete_game.php?game_id='.$row['game_id'].'" role="button" class="btn btn-danger">Delete</a></td></tr>';
     }
 
     ?>
