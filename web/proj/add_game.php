cab7da52b70817baca8d2be9fa0383eba17871f7
spencer <pec15002@byui.edu>
2019-02-04 11:59:08 -0700

    added commands.sql

--- web/proj/add_game.php
+++ web/proj/add_game.php
@@ -3,7 +3,7 @@
 include 'session.php';
 
     $game_name = $_POST["game_name"];
-    $user_id = $_SESSION['user_id']
+    $user_id = $_SESSION['user_id'];
 
 
     $stmt = $db->prepare('INSERT INTO games (user_id, game_name) VALUES  (:user_id, :game_name)');
